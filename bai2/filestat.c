f67facfcd1a83b398ea52aa4943337fb1de71921
Dung <DungNtd@gmail.com>
2025-07-09 11:54:49 +0700

    Change

--- bai2/filestat.c
+++ bai2/filestat.c
@@ -12,13 +12,19 @@ int main(int argc, char *argv[]) {
 
     struct stat info;
     if (lstat(argv[1], &info) != 0) {
-        perror("lstat");
+        perror("Error");
         return 1;
     }
-
     printf("File: %s\n", argv[1]);
     printf("Size: %ld bytes\n", info.st_size);
-    printf("Permissions: %o\n", info.st_mode & 0777);
+    //S_ISREG(), S_ISDIR(), S_ISLNK()
+    if (S_ISLNK(info.st_mode)) {
+    printf("Là symbolic link\n");
+} else if (S_ISDIR(info.st_mode)) {
+    printf("Là thư mục\n");
+} else if (S_ISREG(info.st_mode)) {
+    printf("Là file thường\n");
+}  
     printf("Owner UID: %d\n", info.st_uid);
     printf("Group GID: %d\n", info.st_gid);
     printf("Last Access: %s", ctime(&info.st_atime));
